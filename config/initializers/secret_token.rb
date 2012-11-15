d0959a7fb5743f283852f8f0666c938c86bcf76e
Muni <munendra.chevuru@cognizant.com>
2012-11-15 17:21:10 +0530

    missing files commited - initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PingmeWeb::Application.config.secret_token = '2ca50859ddbacc7bc9b1a2958ed740787fbc906a38c0724a516976d3457180244e6f3bb3e41cc4588dea248121070b2abaae675a11705ade408ca757bf1244ce'
