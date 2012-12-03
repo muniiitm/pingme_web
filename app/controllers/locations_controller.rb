7f9c397f0721392d2d46126f35ab8f95856c6f4e
Karuppasamy <karuppasamy.m@cognizant.com>
2012-12-03 12:58:07 +0530

    replaced pingme api URL

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,8 +9,14 @@ class LocationsController < ApplicationController
 	def create				
     # move to intilizers
     response=HTTParty.post(API_HOST+"/associates/location",{:body=>JSON.parse({:user=>params, :access_token => "#{session[:access_token]}"}.to_json)})
-    response = JSON.parse(response.body)         
-    flash[:notice] = APP_MESSAGE["location_success"] if response["status"] == "success"
-    redirect_to locations_path
+    response = JSON.parse(response.body)    
+    
+    if response["status"] == "success"
+	    flash[:notice] = APP_MESSAGE["location_success"] 
+	    redirect_to locations_path
+  	else
+  		flash[:notice] = APP_MESSAGE["location_failed"] 
+  		redirect_to new_sessions_path
+  	end
 	end
 end
